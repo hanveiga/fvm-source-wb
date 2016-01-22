5008417ec53d007cf4f0039c245d5dc429d96ed5
hanveiga <han.veiga@gmail.com>
2016-01-22 13:42:31 +0100

    added compute_update_sr routine. TODO: BCs

--- benchmark.f90
+++ benchmark.f90
@@ -30,7 +30,7 @@ subroutine get_x(x,size)
   real(kind=8)::dx, xcell
   integer::i
 
-  dx = 1/dble(nx)
+  dx = boxlen/dble(nx)
   do i=1,nx
     x(i) = (i-0.5)*dx
   end do
@@ -54,7 +54,11 @@ subroutine get_initial_conditions(x,u,size)
     case(2)
       w(1,:) = exp(-x)
       w(2,:) = 0
-      w(3,:) = exp(-x)+0.0001*exp(-100*(x-0.5)**2)
+      w(3,:) = exp(-x)+0.01*exp(-100*(x-0.5)**2)
+    case(3)
+      w(1,:) = (1 - ((gamma - 1)/gamma)*1*x)**(1/(gamma-1))
+      w(2,:) = 0
+      w(3,:) = (1 - ((gamma - 1)/gamma)*1*x)**(gamma/(gamma-1))
   end select
 
   call compute_conservative(w,u,nx)
@@ -74,7 +78,7 @@ subroutine output_file(x_values, function_values, filen)
   real(kind=8)::dx, xcell
   real(kind=8),dimension(1:nvar)::primitives
   integer::one
-  dx = 1/dble(nx)
+  dx = boxlen/dble(nx)
   one = 1
 !  write(filen,"(A5,I5.5)")
   open(10,file=TRIM(filen)//".dat",form='formatted')
@@ -122,6 +126,7 @@ subroutine get_equilibrium_solution(x,w, size)
   integer::size
   real(kind=8),dimension(1:nvar,1:size)::w
   real(kind=8),dimension(1:size)::x
+  !class(:)::x
   ! internal variables
 
   select case (nequilibrium)
@@ -133,6 +138,10 @@ subroutine get_equilibrium_solution(x,w, size)
       w(1,:) = exp(-x)
       w(2,:) = 0
       w(3,:) = exp(-x)
+    case(3)
+      w(1,:) = (1 - ((gamma - 1)/gamma)*1*x)**(1/(gamma-1))
+      w(2,:) = 0
+      w(3,:) = (1 - ((gamma - 1)/gamma)*1*x)**(gamma/(gamma-1))
   end select
 
 end subroutine get_equilibrium_solution
@@ -193,7 +202,7 @@ subroutine evolve(u, u_eq)
   real(kind=8),dimension(1:nvar,1:nx)::dudt, w, w1
   integer::iter, n, i
 
-  dx = 1/dble(nx)
+  dx = boxlen/dble(nx)
 
   t=0
   iter=0
@@ -218,6 +227,14 @@ subroutine evolve(u, u_eq)
        u=0.5*u+0.5*w1+0.5*dt*dudt
      endif
 
+     if(solver=='WB1')then
+       ! runge kutta 2nd order
+       call compute_update_sr(u,u_eq, dudt)
+       w1=u+dt*dudt
+       call compute_update_sr(u,u_eq,dudt)
+       u=0.5*u+0.5*w1+0.5*dt*dudt
+     endif
+
      t=t+dt
      iter=iter+1
      write(*,*)'time=',iter,t,dt
@@ -238,13 +255,13 @@ subroutine compute_update(u, w_eq, dudt)
   real(kind=8),dimension(1:nvar,1:(nx+1))::u_eq_faces, flux_riemann, flux_eq, w_eq_faces
   real(kind=8),dimension(1:nvar, 1:nx)::u_left,u_right, u_eq
   real(kind=8),dimension(1:nvar,1:nx)::s,s_eq
-  real(kind=8)::oneoverdx, dx, dt, dx_plus, xcell, zero, xface
+  real(kind=8)::oneoverdx, dx, dt, dx_plus, zero, xface, xcell
   real(kind=8),dimension(1:nvar)::flux_r, u_minus, u_plus, f_plus, f_minus, w_plus, w_minus, u_face, w_face
   integer::one, i, nfaces, iface, ileft, iright
-  dx = 1/dble(nx)
+  dx = boxlen/dble(nx)
   oneoverdx = 1/dx
   nfaces = nx + 1
-  dx_plus = 1/dble(nfaces)
+  dx_plus = boxlen/dble(nfaces)
   zero = 0
   ! compute perturbation
   call compute_conservative(w_eq,u_eq, nx)
@@ -305,7 +322,10 @@ subroutine compute_update(u, w_eq, dudt)
       xcell = 0 !dx !(0-0.5)*dx
       !xface = dx
       !w_face = (/ exp(-xface), dble(0), exp(-xface)/)
-      w_minus(1:nvar) = (/ exp(-xcell), dble(0), exp(-xcell)/)
+      !w_minus(1:nvar) = (/ exp(-xcell), dble(0), exp(-xcell)/)
+      w_minus(1:nvar) = (/ (1 - ((gamma - 1)/gamma)*1*xcell)**(1/(gamma-1)), dble(0),&
+      & (1 - ((gamma - 1)/gamma)*1*xcell)**(gamma/(gamma-1))/)
+      !call get_equilibrium_solution(xcell,w_minus, one)
       w_face = w_minus
       call compute_conservative(w_minus, u_minus, one)
       call compute_conservative(w_face, u_face, one)
@@ -315,7 +335,10 @@ subroutine compute_update(u, w_eq, dudt)
     end if
     if (bc==3.and.iface==nx+1) then
             xcell = nx*dx !(nx+1-0.5)*dx
-            w_plus(1:nvar) = (/ exp(-xcell), dble(0), exp(-xcell)/)
+            !w_plus(1:nvar) = (/ exp(-xcell), dble(0), exp(-xcell)/)
+            w_plus(1:nvar) = (/ (1 - ((gamma - 1)/gamma)*1*xcell)**(1/(gamma-1)), dble(0)&
+            &, (1 - ((gamma - 1)/gamma)*1*xcell)**(gamma/(gamma-1))/)
+            !call get_equilibrium_solution(xcell,w_minus, one)
             call compute_conservative(w_plus, u_plus, one)
             call compute_flux(u_plus+delta_w(1:nvar,nx), f_plus, one)
             call compute_llflux(u_right(1:nvar,ileft)+delta_w(1:nvar,nx), u_plus, f_right(1:nvar,ileft), &
@@ -392,10 +415,10 @@ subroutine compute_update_fvm(u, w_eq, dudt)
   real(kind=8)::oneoverdx, dx, dt, dx_plus, xcell, zero, xface
   real(kind=8),dimension(1:nvar)::flux_r, u_minus, u_plus, f_plus, f_minus, w_plus, w_minus, u_face, w_face
   integer::one, i, nfaces, iface, ileft, iright
-  dx = 1/dble(nx)
+  dx = boxlen/dble(nx)
   oneoverdx = 1/dx
   nfaces = nx + 1
-  dx_plus = 1/dble(nfaces)
+  dx_plus = boxlen/dble(nfaces)
   zero = 0
   ! compute perturbation
   call compute_conservative(w_eq,u_eq, nx)
@@ -472,3 +495,136 @@ subroutine compute_update_fvm(u, w_eq, dudt)
 
 
 end subroutine compute_update_fvm
+!-----------------------------------------
+
+
+subroutine compute_update_sr(u, w_eq, dudt)
+  use parameters
+  implicit none
+  real(kind=8),dimension(1:nvar,1:nx)::u, w_eq, dudt
+
+  ! internal vars
+  real(kind=8),dimension(1:nvar,1:nx)::delta_w, f_left, f_right
+  real(kind=8),dimension(1:(nx+1))::x_faces
+  real(kind=8),dimension(1:(nx))::x
+  real(kind=8),dimension(1:nvar,1:(nx+1))::u_eq_faces, flux_riemann, flux_eq, w_eq_faces
+  real(kind=8),dimension(1:nvar, 1:nx)::u_left,u_right, u_eq, w_left, w_right, w
+  real(kind=8),dimension(1:nvar,1:nx)::s,s_eq
+  real(kind=8)::oneoverdx, dx, dt, dx_plus, xcell, zero, xface, w_temp
+  real(kind=8),dimension(1:nvar)::flux_r, u_minus, u_plus, f_plus, f_minus, w_plus, w_minus, u_face, w_face
+  integer::one, i, nfaces, iface, ileft, iright
+  real(kind=8),dimension(1:nvar)::w_n, w_eq_t, w_plus_t
+  real(kind=8)::phi, x_c, x_minus, x_plus
+  dx = boxlen/dble(nx)
+  oneoverdx = boxlen/dx
+
+  nfaces = nx + 1
+  dx_plus = 1/dble(nfaces)
+
+  zero = 0
+
+  ! create x_faces
+  ! create x
+  do i = 1,nx
+    x(i) = (i-0.5)*dx
+  end do
+  do i = 1,nfaces
+    x_faces(i) = (i-1)*dx
+  end do
+
+  call compute_primitive(u,w,nx)
+
+  ! compute u left, u right with equilibrium formula
+  w_left(1:2,:) = w(1:2,:)
+  w_right(1:2,:) = w(1:2,:)
+  do i = 1,nx
+    x_c = i*dx
+    x_minus = (i-1)*dx
+    x_plus = (i+1)*dx
+    w_left(3,i) = w(3,i) + w(1,i) * (phi(x_c) - phi(x_minus))/2
+    w_right(3,i) = w(3,i) - w(1,i) * (phi(x_plus) - phi(x_c))/2
+  end do
+
+  call compute_conservative(w_left,u_left,nx)
+  call compute_conservative(w_right,u_right,nx)
+
+  ! compute fluxes
+  call compute_flux(u_left, f_left, nx)
+  call compute_flux(u_right, f_right, nx)
+
+  one = 1
+  ! compute numerical flux
+  do iface = 1,nfaces
+    ileft = iface - 1
+    iright = iface
+    if (bc==1) then ! periodic
+      if(iface==1) ileft = nx
+      if(iface==nx+1) iright = 1
+    end if
+    if (bc==2) then ! zero gradient
+      if(iface==1) ileft = 1
+      if(iface==nx+1) iright = nx
+    end if
+    if (bc==3) then ! ! include reflexive BC
+      if(iface==1) ileft = 1
+      if(iface==nx+1) iright = nx
+    end if
+
+    call compute_llflux(u_right(1:nvar,ileft),u_left(1:nvar,iright), f_right(1:nvar,ileft), &
+                    & f_left(1:nvar,iright), flux_r)
+    ! treat bc
+    flux_riemann(1:nvar,iface) = flux_r
+     if (bc==3.and.iface==1) then
+       !xcell = 0 !-0.5*dx
+       x_c = 0 !-0.5*dx
+       x_minus = -0.5*dx
+       w_minus(1:nvar) = (/ (1 - ((gamma - 1)/gamma)*1*x_c)**(1/(gamma-1)), dble(0),&
+       & dble(0) /)
+       w_temp = (1 - ((gamma - 1)/gamma)*1*x_c)**(gamma/(gamma-1))
+       w_minus(3) = w_temp + w_minus(1)*(phi(x_c) - phi(x_minus))/2
+       call compute_primitive(u(1:nvar,1),w_n,one)
+       w_eq_t(1:nvar) = (/ (1 - ((gamma - 1)/gamma)*1*(0)*dx)**(1/(gamma-1)),&
+              &dble(0),&
+              & (1 - ((gamma - 1)/gamma)*1*(0)*dx)**(gamma/(gamma-1))/)
+       w_minus = w_minus !+ (w_n - w_eq_t)
+
+       call compute_conservative(w_minus, u_minus, one)
+       call compute_flux(u_minus, f_minus, one)
+       call compute_llflux(u_minus, u_left(1:nvar,iright), f_minus, &
+                       & f_left(1:nvar,iright), flux_riemann(1:nvar,iface))
+     end if
+     if (bc==3.and.iface==nx+1) then
+      !xcell = nx*dx !(nx+0.5)*dx !(nx+1-0.5)*dx
+      x_plus = (nx+0.5)*dx
+      x_c = (nx)*dx
+      w_plus_t(1:nvar) = (/ (1 - ((gamma - 1)/gamma)*1*x_c)**(1/(gamma-1)), dble(0),&
+      & dble(0)/)
+      w_temp = (1 - ((gamma - 1)/gamma)*1*x_c)**(gamma/(gamma-1))
+      w_plus_t(3) = w_temp - w_plus(1) * (phi(x_plus) - phi(x_c))/2
+      call compute_primitive(u(1:nvar,nx),w_n,one)
+      w_eq_t(1:nvar) = (/ (1 - ((gamma - 1)/gamma)*1*(nx)*dx)**(1/(gamma-1)),&
+                    &dble(0),&
+                    & (1 - ((gamma - 1)/gamma)*1*(nx)*dx)**(gamma/(gamma-1))/)
+      w_plus = w_plus_t !+ (w_n - w_eq_t)
+      call compute_conservative(w_plus, u_plus, one)
+      call compute_flux(u_plus, f_plus, one)
+      call compute_llflux(u_right(1:nvar,ileft), u_plus, f_right(1:nvar,ileft), &
+                      & f_plus, flux_riemann(1:nvar,iface))
+     end if
+
+  end do
+
+  ! compute source
+  call get_source(u,s,x, nx)
+
+  dudt(1:nvar,1:nx) = -(flux_riemann(1:nvar,2:(nx+1))-flux_riemann(1:nvar,1:nx))*oneoverdx + s(1:nvar,1:nx)
+
+end subroutine compute_update_sr
+
+function phi(x) result(phi_m)
+  ! assuming linear phi
+  real(kind=8)::g, x, phi_m
+  g = 1
+  phi_m = g*x
+
+end function phi
